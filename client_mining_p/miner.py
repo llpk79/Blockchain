dcbc47109cb0e1d82fe870cddd81a8d38c401166
Paulus Kutrich <pkutrich@gmail.com>
2019-12-05 03:25:35 -0800

    multiprocessing and modularization

--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -1,45 +1,21 @@
-from hashlib import blake2b
-import requests
-from uuid import uuid4
-from timeit import default_timer
-import random
-import sys
 import json
+import os
+import random
+import requests
+import sys
+from hashlib import blake2b
+from multiprocessing import Pool, cpu_count
+from timeit import default_timer
+from uuid import uuid4
 
-
-SECRET_KEY = b'super secret key'
+SECRET_KEY = os.environ.get('SECRET_KEY') or b'super secret key'
 AUTH_SIZE = 32
-
-
-def proof_of_work(block):
-    """
-    Simple Proof of Work Algorithm
-    Stringify the block and look for a proof.
-    Loop through possibilities, checking each one against `valid_proof`
-    in an effort to find a number that is a valid proof
-    :return: A valid proof for the provided block
-    """
-    x = 0
-    block_string = json.dumps(block)
-    status = ''
-    dash = True
-    while True:
-        if valid_proof(block_string, x):
-            return x
-        x += 1
-        if x % 100000 == 0:
-            if dash:
-                status += '-'
-                dash = False
-                # Test transaction endpoint.
-                do_a_transaction()
-            else:
-                status += '*'
-                dash = True
-            print(status, end="\r")
-        if x % 10000000 == 0:
-            status = ''
-            print(status)
+URL = "http://localhost:5000"
+PROCESSES = cpu_count()
+r = requests.get(url=URL + '/zeros')
+NUM_ZEROS = r.json()['zeros']
+print('\ncpu_count:', PROCESSES)
+print(f'Zero difficulty: {NUM_ZEROS}')
 
 
 def valid_proof(block_string, proof):
@@ -57,79 +33,140 @@ def valid_proof(block_string, proof):
     proof_string = block_string + str(proof)
     hasher = blake2b(key=SECRET_KEY, digest_size=AUTH_SIZE)
     hasher.update(proof_string.encode('utf8'))
-    hash = hasher.hexdigest()
-    n = 5
-    if hash[:n] == '0' * n:
+    hash_ = hasher.hexdigest()
+    if hash_[:NUM_ZEROS] == '0' * NUM_ZEROS:
         return True
     return False
 
 
+def get_data():
+    r = requests.get(url=node + "/last_block")
+    # Handle non-json response
+    try:
+        data = r.json()
+    except ValueError:
+        print("Error:  Non-json response")
+        print("Response returned:")
+        print(r)
+        return
+    return data
+
+
 def do_a_transaction():
-    post_data = {'amount': random.randint(0, 100),
-                 'recipient': random.randint(0, 100),
-                 'sender': random.randint(0, 100)}
-    requests.post(url='http://localhost:5000' + '/transactions/new', json=post_data)
+    requests.post(url=node + '/transactions/new', json={'amount': random.randint(0, 100),
+                                                        'sender': random.randint(0, 100),
+                                                        'recipient': random.randint(0, 100)})
+
+
+def status_update(x, dash, status):
+    if x % 100000 == 0:
+        if dash:
+            status += '-'
+            do_a_transaction()
+            dash = False
+        else:
+            status += '*'
+            dash = True
+        print(status, end='\r')
+    if x % 10000000 == 0:
+        status = '$'
+        print(status)
+    return dash
+
+
+def final_status(coins_mined, guess_rates, total_time):
+    print('\nSession finished.')
+    print(f"Total coins mined this session: {coins_mined}")
+    print(f'Average guess rate average this session: {sum(guess_rates) / len(guess_rates):.2f}/sec')
+    print(f'High average guess rate: {max(guess_rates):.2f}/sec '
+          f'Low average guess rate: {min(guess_rates):.2f}/sec')
+    print(f'Average time per block: {total_time / coins_mined:.2f} sec')
+    print(f'Total time this session: {total_time:.2f} sec')
+
+
+def get_node():
+    if len(sys.argv) > 1:
+        node = sys.argv[1]
+    else:
+        node = URL
+    return node
+
+
+def get_id():
+    try:
+        f = open("../my_id.txt", "r")
+        id_ = f.read()
+        print("ID is", id_)
+        f.close()
+
+    except FileNotFoundError:
+        id_ = str(uuid4()).replace('-', '')
+        print("ID is", id_)
+        f = open("my_id.txt", "w")
+        f.write(id_)
+        f.close()
+    return id_
+
+
+def block_update(coins_mined, guess_rate, coin_time):
+    print(f'\n** {message} **')
+    print(f"Total coins mined this session: {coins_mined}")
+    print(f'Guess rate for this block: {guess_rate:.2f}/sec')
+    print(f"Time to mine last block: {coin_time:.2f} sec")
+    print(f'Average time per block: {total_time / coins_mined:.2f} sec')
+    print(f'Total time this session: {total_time:.2f} sec\n')
 
 
 if __name__ == '__main__':
     # What is the server address? IE `python3 miner.py https://server.com/api/`
-    if len(sys.argv) > 1:
-        node = sys.argv[1]
-    else:
-        node = "http://localhost:5000"
-
-    # Load ID
     try:
-        f = open("my_id.txt", "r")
-        id = f.read()
-        print("ID is", id)
-        f.close()
-    except FileNotFoundError:
-        id = str(uuid4()).replace('-', '')
-        print("ID is", id)
-        f = open("my_id.txt", "w")
-        f.write(id)
-        f.close()
+        with Pool() as pool:
+            node = get_node()
+            id_ = get_id()
 
-    coins_mined = 0
+            coins_mined, total_time = 0, 0
+            guess_rates = []
 
-    # Run forever until interrupted
-    while True:
-        then = default_timer()
-        print('Mining...')
-        r = requests.get(url=node + "/last_block")
-        # Handle non-json response
-        try:
-            data = r.json()
-        except ValueError:
-            print("Error:  Non-json response")
-            print("Response returned:")
-            print(r)
-            break
+            # Run forever until interrupted
+            while True:
+                then = default_timer()
+                print('Mining next block...  "-*" = 200,000 guesses')
+                data = get_data()
 
-        # Get the block from `data` and use it to look for a new proof
-        block = data['last_block']
-        new_proof = proof_of_work(block)
+                # Get the block from `data` and use it to look for a new proof
+                block = json.dumps(data['last_block'])
+                x, guesses = 0, 100000
+                status, dash = '$', True
+                print(status, end='\r')
+                while True:
+                    new_proof = pool.starmap(valid_proof, [(block, guess) for guess in range(x, x + guesses)],
+                                             chunksize=guesses // PROCESSES)
+                    new_proof_ = [x for x, guess in enumerate(new_proof, start=x) if guess]
+                    if new_proof_:
+                        break
+                    x += guesses
+                    dash = status_update(x, dash, status)
 
-        # When found, POST it to the server {"proof": new_proof, "id": id}
-        post_data = {"proof": new_proof, "id": id}
+                # When found, POST it to the server {"proof": new_proof, "id": id}
+                post = {"proof": new_proof_[0], "id": id_}
+                r = requests.post(url=node + "/mine", json=post)
+                data = r.json()
 
-        r = requests.post(url=node + "/mine", json=post_data)
-        data = r.json()
+                # If the server responds with a 'message' 'New Block Forged'
+                # add 1 to the number of coins mined and print it.  Otherwise,
+                # print the message from the server.
+                message = data['message']
+                if message == 'New Block Forged':
+                    coins_mined += 1
+                    now = default_timer()
+                    coin_time = now - then
+                    total_time += coin_time
+                    guess_rate = new_proof_[0] / coin_time
+                    guess_rates.append(guess_rate)
+                    block_update(coins_mined, guess_rate, coin_time)
+                else:
+                    print(message)
+    except KeyboardInterrupt:
+        final_status(coins_mined, guess_rates, total_time)
+        sys.exit(0)
 
-        # If the server responds with a 'message' 'New Block Forged'
-        # add 1 to the number of coins mined and print it.  Otherwise,
-        # print the message from the server.
-        message = data['message']
-        if message == 'New Block Forged':
-            coins_mined += 1
-            print('\n', message)
-            print(f"Total coins mined this session: {coins_mined}")
-            now = default_timer()
-            print(f"Time to mine last coin: {now - then:.2f} seconds.")
-            if coins_mined == 20:
-                r = requests.get(url=node + "/chain")
-                print(r.json())
-                break
-        else:
-            print(message)
